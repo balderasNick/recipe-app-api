88169867e5e5c744a7c1425fceae2b27901203c4
Nicholas Balderas <nicholasbalderas@Nicholass-MacBook-Pro.local>
2019-04-19 10:56:03 -0500

    fixed flake8 issue

--- app/core/admin.py
+++ app/core/admin.py
@@ -16,7 +16,6 @@ class UserAdmin(BaseUserAdmin):
             {
                 'fields': ('is_active', 'is_staff', 'is_superuser')
             }
-
         ),
         (_('Important dates'), {'fields': ('last_login',)})
     )
